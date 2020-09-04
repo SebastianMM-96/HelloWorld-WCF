ba4194df82ec824e083f27efcbc367d647843556
Sebastián Marroquín Martínez <sebasmarro10@gmail.com>
2020-09-04 11:38:51 -0500

    Add files via upload

---
+++ RemotingServiceHost/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+
+
+namespace RemotingServiceHost
+{
+    class Program
+    {
+        static void Main()
+        {
+            HelloRemotingService.HelloRemotingService remotingService = new HelloRemotingService.HelloRemotingService();
+            //Crear TCP Channel
+            TcpChannel channel = new TcpChannel(8080);
+            ChannelServices.RegisterChannel(channel);
+            RemotingConfiguration.RegisterWellKnownServiceType(
+                typeof(HelloRemotingService.HelloRemotingService),
+                "GetMessage", WellKnownObjectMode.Singleton);
+            Console.WriteLine("Servicio remoto iniciado @ " + DateTime.Now);
+            Console.WriteLine("Presiona <ENTER> para salir!");
+            Console.ReadLine();
+        }
+    }
+}
